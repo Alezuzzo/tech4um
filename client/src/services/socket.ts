db0a836d75a932e430b2198b1dbedba1dd637a99
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-11-28 17:15:30 -0300

    Configuração para deploy do front

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,9 +1,11 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'https://tech4um.onrender.com';
 
 const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
+  transports: ['websocket'],
+  reconnection: true,
 });
 
 export default socket;
